c0f76683f28e9a934d890d3f31a3703bdc1daf91
zorodesign <zorodesign@gmail.com>
2013-04-04 12:19:10 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weichuban::Application.config.secret_token = '5557b3c24e86560046e330e9b3b5450015b0eddb6706a41bef9c2fe3c606501ce5ab0b2511882f9235b0820fb873e029616dc7c24b72028770fb62177378ceb6'
